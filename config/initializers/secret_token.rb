b7ccf14f59d43d4929c6686b62ab38132a3fc48a
Michelle Caers <mcaers@gmail.com>
2013-04-08 14:37:00 -0400

    Initial commit of robot and UI interaction tool

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+McHammer::Application.config.secret_token = '92c56ef5c8b9a76d3d62cb9a31c03f9274be9c3a9a577b1d2f382d8251246a4f6a9c9276e56654a152aea56ba46553d46d3d564ac4c6a42a602052e5d214e0d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+McHammer::Application.config.session_store :cookie_store, key: '_mc_hammer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# McHammer::Application.config.session_store :active_record_store
